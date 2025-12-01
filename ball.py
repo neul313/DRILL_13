5fd56264e7f8be23812359bca202a7974c4ad92e
Jo shinBe <tls3123@tukorea.ac.kr>
2025-12-01 12:44:10 +0900

    기본 정보 넣기 이미지, 그려질 위치, 디버깅박스

--- ball.py
+++ ball.py
@@ -4,13 +4,22 @@ import common
 import random
 
 class Ball():
-    def __init__(self):
-        pass
+    image = None
+
+    def __init__(self,x, y):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+            self.x, self.y = x, y
+
     def draw(self):
-        pass
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
     def update(self):
         pass
+
     def get_bb(self):
-        pass
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
     def handle_collision(self, group, other):
         pass
