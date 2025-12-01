35367e2f838b8491302affe99b71129f8023b8ef
Jo shinBe <tls3123@tukorea.ac.kr>
2025-12-01 12:42:23 +0900

    뼈대 완성

---
+++ ball.py
@@ -0,0 +1,16 @@
+from pico2d import *
+import game_world
+import common
+import random
+
+class Ball():
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def update(self):
+        pass
+    def get_bb(self):
+        pass
+    def handle_collision(self, group, other):
+        pass
